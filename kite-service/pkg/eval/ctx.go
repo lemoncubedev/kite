770611725b364cf352182646059183a58d3bc807
LemonCube <lemoncube@lemoncube.dev>
2026-05-18 10:49:55 -0700

    Add Position field to RoleEnv struct
    Added Position field to RoleEnv struct and updated NewRoleEnv function.

--- kite-service/pkg/eval/ctx.go
+++ kite-service/pkg/eval/ctx.go
@@ -411,18 +411,20 @@ func (c ChannelEnv) String() string {
 type RoleEnv struct {
 	og discord.Role
 
-	ID      string `expr:"id" json:"id"`
-	Name    string `expr:"name" json:"name"`
-	Mention string `expr:"mention" json:"mention"`
+	ID       string `expr:"id" json:"id"`
+	Name     string `expr:"name" json:"name"`
+	Mention  string `expr:"mention" json:"mention"`
+	Position string `expr:"postition" json:"position"`
 }
 
 func NewRoleEnv(role discord.Role) *RoleEnv {
 	return &RoleEnv{
 		og: role,
 
-		ID:      role.ID.String(),
-		Name:    role.Name,
-		Mention: fmt.Sprintf("<@&%s>", role.ID.String()),
+		ID:       role.ID.String(),
+		Name:     role.Name,
+		Mention:  fmt.Sprintf("<@&%s>", role.ID.String()),
+		Position: role.Position.String(),
 	}
 }
 
